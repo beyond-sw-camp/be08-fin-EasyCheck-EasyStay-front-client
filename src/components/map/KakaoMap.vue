24814897409239e117ad4a55deab4049b1c88d63
Jihoon Lim <bo9701@naver.com>
2024-10-16 02:03:40 +0900

    design: 카카오맵 띄우기 성공

---
+++ src/components/map/KakaoMap.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    mounted() {
+        const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+        const mapOption = {
+            center: new window.kakao.maps.LatLng(37.5665, 126.9780), // 지도 중심좌표
+            level: 3 // 확대 레벨
+        };
+        const map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도 생성
+    }
+};
+</script>
+
+<template>
+    <div class="mb-5" id="map" style="width:100%; height:500px;"></div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
