e9c1b4a8330d15f40ca30bd901686de1a133315c
Jihoon Lim <bo9701@naver.com>
2024-10-16 11:38:40 +0900

    refactor: appkey 삭제

--- src/components/map/KakaoMap.vue
+++ src/components/map/KakaoMap.vue
@@ -1,16 +1,14 @@
-<script>
-export default {
-    mounted() {
-        const mapContainer = document.getElementById('map'); // 지도를 표시할 div
-        const mapOption = {
-            center: new window.kakao.maps.LatLng(37.5665, 126.9780), // 지도 중심좌표
-            level: 3 // 확대 레벨
-        };
-        const map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도 생성
-    }
+<script setup>
+import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps';
+const coordinate = {
+    lat: 37.566826,
+    lng: 126.9786567
 };
 </script>
 
 <template>
-    <div class="mb-5" id="map" style="width:100%; height:500px;"></div>
+    <KakaoMap :lat="coordinate.lat" :lng="coordinate.lng" :draggable="true" class="mb-5"
+        style="width:100%; height:500px;">
+        <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng"></KakaoMapMarker>
+    </KakaoMap>
 </template>
