f5f2354d5dd6c186dc25a853762026def98ac67e
hoyeon96 <hoyen96@naver.com>
2024-10-17 18:05:34 +0900

    feat: ThemePark View페이지 디자인

---
+++ src/views/ThemeParks/ThemeParkView.vue
@@ -0,0 +1,40 @@
+<template>
+  <navbar-default :sticky="true"/>
+  <ThemeParkInfo />
+  <div class="container-fluid px-4">
+    <div class="section-divider"></div>
+    <attraction-info />
+  </div>
+</template>
+
+<script setup>
+import NavbarDefault from "@/examples/navbars/NavbarDefault.vue";
+import ThemeParkInfo from "@/views/ThemeParks/ThemeParkInfo.vue";
+import AttractionInfo from "@/views/ThemeParks/AttractionInfo.vue";
+</script>
+
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.section-divider {
+  height: 1px;
+  background: linear-gradient(to right, transparent, #ccc, transparent);
+  margin: 3rem 0;
+  position: relative;
+
+  &::before {
+    content: "●";
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    color: #ccc;
+    padding: 0 10px;
+    font-size: 0.8rem;
+  }
+}
+</style>
