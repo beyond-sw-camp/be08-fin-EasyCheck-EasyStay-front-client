781d7fa43e04cad2465233c770f4c4ae6e743d72
yongun2 <djkye0001@naver.com>
2024-10-24 12:43:13 +0900

    chore: 객실 디테일 페이지 라우팅 수정

--- src/router/index.js
+++ src/router/index.js
@@ -177,7 +177,7 @@ const router = createRouter({
       component: ElTypography,
     },
     {
-      path: "/accommodation",
+      path: "/accommodation/:accommodationId",
       name: "Accommodation",
       component: AccommodationView,
       props: (route) => ({
