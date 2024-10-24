1d756997bc9cf2fbffe77a259e4daef7da489c9e
yongun2 <djkye0001@naver.com>
2024-10-24 12:42:05 +0900

    feat: 메인 화면에서 숙박시설 눌렀을 때 디테일 페이지 이동

--- src/views/Presentation/Sections/Accommodation/AccommodationList.vue
+++ src/views/Presentation/Sections/Accommodation/AccommodationList.vue
@@ -6,40 +6,51 @@ import ExampleCard from "../../Components/ExampleCard.vue";
 const accommodations = ref([]);
 
 const fetchAccommodations = async () => {
-    try {
-        const response = await apiClient.get('/accommodations');
-        accommodations.value = response.data;
-    } catch (error) {
-        console.error("Failed to fetch accommodations:", error);
-    }
+  try {
+    const response = await apiClient.get("/accommodations");
+    accommodations.value = response.data;
+  } catch (error) {
+    console.error("Failed to fetch accommodations:", error);
+  }
 };
 
 onMounted(() => {
-    fetchAccommodations();
+  fetchAccommodations();
 });
 </script>
 
 <template>
-    <section class="mb-5 pb-5">
-        <div class="container mt-sm-5 mt-3">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="position-sticky pb-lg-5 pb-3 mt-lg-0 mt-5 ps-2" style="top: 100px">
-                        <h3>Accommodation</h3>
-                        <h6 class="text-secondary font-weight-normal pe-3">
-                            숙소 목록을 확인하세요.
-                        </h6>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="row">
-                        <div class="col-md-4" v-for="accommodation in accommodations" :key="accommodation.id">
-                            <ExampleCard :image="accommodation.thumbnailUrls[0]" :title="accommodation.name"
-                                :route="'Accommodation'" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <section class="mb-5 pb-5">
+    <div class="container mt-sm-5 mt-3">
+      <div class="row">
+        <div class="col-lg-3">
+          <div
+            class="position-sticky pb-lg-5 pb-3 mt-lg-0 mt-5 ps-2"
+            style="top: 100px"
+          >
+            <h3>Accommodation</h3>
+            <h6 class="text-secondary font-weight-normal pe-3">
+              숙소 목록을 확인하세요.
+            </h6>
+          </div>
         </div>
-    </section>
+        <div class="col-lg-9">
+          <div class="row">
+            <div
+              class="col-md-4"
+              v-for="accommodation in accommodations"
+              :key="accommodation.id"
+            >
+              <ExampleCard
+                :accommodationId="accommodation.id"
+                :image="accommodation.thumbnailUrls[0]"
+                :title="accommodation.name"
+                :route="'Accommodation'"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
