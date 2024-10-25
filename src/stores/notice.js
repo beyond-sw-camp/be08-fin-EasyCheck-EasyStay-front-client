9f2130e03ca01ace4ec7c848e6c3347d4aa8ee98
yongun2 <djkye0001@naver.com>
2024-10-25 12:17:01 +0900

    feat: 공지사항

--- src/stores/notice.js
+++ src/stores/notice.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+// 1번
 import axios from "axios";
 
 export const useNoticeStore = defineStore("notice", {
@@ -7,16 +8,46 @@ export const useNoticeStore = defineStore("notice", {
     accommodations: [], // 리조트 정보를 저장할 배열
     loading: false,
     error: null,
+
+    query: {
+      branch: null,
+      content: null,
+    },
   }),
+  getters: {
+    filteredNotices: (state) => {
+      return (
+        state.notices.filter((notice) => {
+          // 지점(branch) 조건 체크 - 정확히 일치
+          const branchMatch =
+            !state.query.branch || notice.branch === state.query.branch;
+
+          // title 또는 content 조건 체크 (둘 중 하나라도 일치하면 OK)
+          const searchMatch =
+            !state.query.content ||
+            (state.query.content && notice.title.includes(state.query.title)) ||
+            (state.query.content &&
+              notice.content.includes(state.query.content));
+
+          // branch 조건과 (title OR content) 조건을 모두 만족해야 함
+          return branchMatch && searchMatch;
+        }) || []
+      );
+    },
+  },
   actions: {
-    async fetchNotices(accommodationId) {
+    setQueryBranch(branch) {
+      this.query.branch = branch;
+    },
+    setQueryContent(content) {
+      this.query.content = content;
+    },
+    async fetchNotices() {
       this.loading = true;
       this.error = null;
 
       try {
-        const response = await axios.get(`/api/v1/notices-reply`, {
-          params: { accommodation: accommodationId },
-        });
+        const response = await axios.get(`/api/v1/notices-reply`);
         console.log("사업장 : ", response);
         this.notices = response.data; // API 응답 데이터를 notices에 저장
       } catch (err) {
