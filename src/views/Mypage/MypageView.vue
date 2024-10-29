ff3f872d4c5f33d2c06e7e67ed94de836a001419
Jihoon Lim <bo9701@naver.com>
2024-10-29 21:51:58 +0900

    refactor: Navbar 고정

--- src/views/Mypage/MypageView.vue
+++ src/views/Mypage/MypageView.vue
@@ -6,7 +6,6 @@ import { userLoginStore } from "@/stores/loginStore";
 import { mypageStore } from "@/stores/mypageStore";
 
 // example components
-import NavbarDefault from "@/examples/navbars/NavbarDefault.vue";
 import Header from "@/examples/Header.vue";
 
 // material-input
@@ -43,14 +42,6 @@ import easystayImage from '@/assets/img/easystay.png';
 </script>
 
 <template>
-  <div class="position-sticky z-index-sticky top-0 w-100">
-    <div class="row">
-      <div class="col-12">
-        <NavbarDefault :sticky="true" />
-      </div>
-    </div>
-  </div>
-
   <Header>
     <div class="page-header align-items-start min-vh-90" loading="lazy" style="padding-top: 80px;">
       <span class="mask bg-white opacity-6"></span>
--- src/views/Mypage/ThemeparkReservationList/ThemeparkReservationList.vue
+++ src/views/Mypage/ThemeparkReservationList/ThemeparkReservationList.vue
@@ -9,7 +9,6 @@ import easystayImage from "@/assets/img/easystay.png";
 import setMaterialInput from "@/assets/js/material-input";
 
 // example components
-import NavbarDefault from "@/examples/navbars/NavbarDefault.vue";
 import Header from "@/examples/Header.vue";
 import MaterialInput from "@/components/MaterialInput.vue";
 import MaterialButton from "@/components/MaterialButton.vue";
@@ -49,28 +48,13 @@ const updateBranch = () => {
 </script>
 
 <template>
-  <div class="position-sticky z-index-sticky top-0 w-100">
-    <div class="row">
-      <div class="col-12">
-        <NavbarDefault :sticky="true" />
-      </div>
-    </div>
-  </div>
-
   <Header>
-    <div
-      class="page-header align-items-start min-vh-90"
-      loading="lazy"
-      style="padding-top: 80px"
-    >
+    <div class="page-header align-items-start min-vh-90" loading="lazy" style="padding-top: 80px">
       <span class="mask bg-white opacity-6"></span>
       <div class="container custom-login-container my-auto position-relative">
         <div class="row">
           <div class="col-12">
-            <div
-              class="bg-image"
-              :style="{ backgroundImage: 'url(' + easystayImage + ')' }"
-              style="
+            <div class="bg-image" :style="{ backgroundImage: 'url(' + easystayImage + ')' }" style="
                 background-size: cover;
                 background-position: center;
                 height: 230px;
@@ -78,12 +62,8 @@ const updateBranch = () => {
                 align-items: center;
                 justify-content: center;
                 z-index: 1;
-              "
-            >
-              <div
-                class="py-3 mb-3 text-center"
-                style="background-color: rgba(255, 255, 255, 0.3); width: 100%"
-              >
+              ">
+              <div class="py-3 mb-3 text-center" style="background-color: rgba(255, 255, 255, 0.3); width: 100%">
                 <h3 class="text-black font-weight-bolder mb-0 mt-4 mb-2">
                   테마파크 예약 내역
                 </h3>
@@ -95,59 +75,29 @@ const updateBranch = () => {
                 <!-- 검색바 추가 -->
                 <div class="row mt-4">
                   <div class="col-12 text-center">
-                    <div
-                      class="d-flex justify-content-center align-items-center mb-3"
-                    >
+                    <div class="d-flex justify-content-center align-items-center mb-3">
                       <div class="d-flex align-items-center me-3">
-                        <label
-                          for="resort-select"
-                          class="form-label me-2 mb-0"
-                          style="white-space: nowrap"
-                          >지점 선택</label
-                        >
-                        <select
-                          id="resort-select"
-                          v-model="branchQuery"
-                          class="form-select me-2"
-                          @change="updateBranch"
-                        >
+                        <label for="resort-select" class="form-label me-2 mb-0" style="white-space: nowrap">지점
+                          선택</label>
+                        <select id="resort-select" v-model="branchQuery" class="form-select me-2"
+                          @change="updateBranch">
                           <option value="" disabled>지점을 선택하세요</option>
-                          <option
-                            v-for="branch in noticeStore.accommodations"
-                            :key="branch.id"
-                            :value="branch.name"
-                          >
+                          <option v-for="branch in noticeStore.accommodations" :key="branch.id" :value="branch.name">
                             {{ branch.name }}
                           </option>
                         </select>
                       </div>
 
                       <div class="d-flex align-items-center me-3">
-                        <label
-                          for="stayDuration"
-                          class="me-2 mb-0"
-                          style="white-space: nowrap"
-                          >투숙 기간</label
-                        >
-                        <MaterialInput
-                          type="date"
-                          v-model="checkInDate"
-                          id="checkIn"
-                          class="form-control me-2 input-group-outline"
-                        />
+                        <label for="stayDuration" class="me-2 mb-0" style="white-space: nowrap">투숙 기간</label>
+                        <MaterialInput type="date" v-model="checkInDate" id="checkIn"
+                          class="form-control me-2 input-group-outline" />
                         <span class="mx-2">~</span>
-                        <MaterialInput
-                          type="date"
-                          v-model="checkOutDate"
-                          id="checkOut"
-                          class="form-control input-group-outline"
-                        />
+                        <MaterialInput type="date" v-model="checkOutDate" id="checkOut"
+                          class="form-control input-group-outline" />
                       </div>
 
-                      <MaterialButton
-                        @click="searchReservations"
-                        class="btn btn-primary ms-2 mt-2"
-                        >검색
+                      <MaterialButton @click="searchReservations" class="btn btn-primary ms-2 mt-2">검색
                       </MaterialButton>
                     </div>
                   </div>
@@ -173,9 +123,7 @@ const updateBranch = () => {
           <!-- 테마파크 예약 내역 -->
           <div class="col-12 mt-4">
             <h4 class="text-start ms-3">예약 내역</h4>
-            <div
-              style="border-top: 1px solid #000; width: 100%; margin: 10px auto"
-            ></div>
+            <div style="border-top: 1px solid #000; width: 100%; margin: 10px auto"></div>
             <table class="table table-striped">
               <thead>
                 <tr>
