b565b417e5c4fa7819fc52c3b940495094173e0e
Jihoon Lim <bo9701@naver.com>
2024-11-06 12:30:54 +0900

    refactor: footer 로고 변경

--- src/examples/footers/FooterDefault.vue
+++ src/examples/footers/FooterDefault.vue
@@ -1,5 +1,5 @@
 <script setup>
-import logoDark from "@/assets/img/logo-ct-dark.png";
+import logoDark from "@/assets/img/footerLogo.png";
 import { onMounted, ref } from "vue";
 import { useAccommodationStore } from "@/stores/accommodationStore";
 import { RouterLink } from "vue-router";
@@ -95,28 +95,14 @@ onMounted(async () => {
         <div class="col-md-3 mb-4 ms-auto">
           <div>
             <a :href="brand.route">
-              <img
-                :src="brand.logo"
-                class="mb-3 footer-logo"
-                alt="main_logo"
-                style="filter: brightness(0) invert(1)"
-              />
+              <img :src="brand.logo" class="mb-3 footer-logo" alt="main_logo" />
             </a>
             <h6 class="font-weight-bolder mb-4 text-white">{{ brand.name }}</h6>
           </div>
           <div>
             <ul class="d-flex flex-row ms-n3 nav">
-              <li
-                class="nav-item"
-                v-for="{ icon, link } of socials"
-                :key="link"
-              >
-                <a
-                  class="nav-link pe-1 text-white"
-                  :href="link"
-                  target="_blank"
-                  v-html="icon"
-                >
+              <li class="nav-item" v-for="{ icon, link } of socials" :key="link">
+                <a class="nav-link pe-1 text-white" :href="link" target="_blank" v-html="icon">
                 </a>
               </li>
             </ul>
@@ -124,11 +110,7 @@ onMounted(async () => {
         </div>
 
         <!-- 동적으로 생성된 메뉴 목록 -->
-        <div
-          class="col-md-2 col-sm-6 col-6 mb-4"
-          v-for="{ name, items } of menus"
-          :key="name"
-        >
+        <div class="col-md-2 col-sm-6 col-6 mb-4" v-for="{ name, items } of menus" :key="name">
           <h6 class="text-sm text-white">{{ name }}</h6>
           <ul class="flex-column ms-n3 nav">
             <li class="nav-item" v-for="item in items" :key="item.name">
@@ -145,12 +127,8 @@ onMounted(async () => {
               All rights reserved. Copyright ©
               {{ new Date().getFullYear() }}
               EasyStay by
-              <a
-                href="https://github.com/beyond-sw-camp/be08-fin-EasyCheck-EasyStay-server"
-                target="_blank"
-                class="text-white"
-                >EasyCheck</a
-              >.
+              <a href="https://github.com/beyond-sw-camp/be08-fin-EasyCheck-EasyStay-server" target="_blank"
+                class="text-white">EasyCheck</a>.
             </p>
           </div>
         </div>
@@ -158,9 +136,3 @@ onMounted(async () => {
     </div>
   </footer>
 </template>
-
-<style>
-.footer-logo {
-  filter: brightness(0) invert(1);
-}
-</style>
